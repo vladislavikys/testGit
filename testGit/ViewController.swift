663deb0764db18db9bde692ea14cb36a7e32e3ad
vladislavikys <zarya.vlad@yandex.ru>
2023-09-07 09:58:40 +0300

    Initial Commit

---
+++ testGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testGit
+//
+//  Created by Влад on 7.09.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
