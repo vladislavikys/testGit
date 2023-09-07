513b9375e18966386561a9cbe26762d83e760442
vladislavikys <zarya.vlad@yandex.ru>
2023-09-07 10:15:19 +0300

    source add

--- testGit/ViewController.swift
+++ testGit/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         let one = "1"
+        let source = "true"
     }
 
 
